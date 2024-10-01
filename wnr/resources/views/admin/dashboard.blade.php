623e53aed6ce47c90f9e2ea5202aaaac30cbc0f4
Adna Bumagat <Adna.bumagat.up@phinmaed.com>
2024-10-01 13:08:39 +0800

    seperate admin and user count

--- wnr/resources/views/admin/dashboard.blade.php
+++ wnr/resources/views/admin/dashboard.blade.php
@@ -6,14 +6,27 @@
 <div class="container mt-5">
     <h1>Welcome to the Admin Dashboard</h1>
 
-    <div class="row ml-auto mt-4">
-        <!-- User Count Card -->
+    <div class="row mt-4">
+
+        <!-- Admin Count Card -->
+        <div class="col-md-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Total Admins</h5>
+                    <p class="card-text">
+                        <strong>{{ $adminCount }}</strong>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Regular Users Count Card -->
         <div class="col-md-4">
             <div class="card text-center">
                 <div class="card-body">
                     <h5 class="card-title">Total Users</h5>
                     <p class="card-text">
-                        <strong>{{ $userCount }}</strong>
+                        <strong>{{ $regularUserCount }}</strong>
                     </p>
                 </div>
             </div>
